59495b70edf33c4314c1a8e7aa367fe487957e6f
hyenachi <hyenachi@nate.com>
2022-04-09 07:26:55 +0900

    create

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} />
+      <h2> hhhhhh </h2>
+    </div>
+  );
+}
+
+export default App;
